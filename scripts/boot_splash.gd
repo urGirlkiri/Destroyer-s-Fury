4e131c3b6c72eed3cc7341d37642a8193518d299
Kiri <cultivatingghoul@gmail.com>
2025-12-01 22:12:02 +0200

    added a glowing head at the end of the progress bar

--- scripts/boot_splash.gd
+++ scripts/boot_splash.gd
@@ -1,18 +1,54 @@
 extends Control
 
-const MAIN = preload("uid://c2g0pw5amocod")
+const MAIN_GAME_SCENE = preload("uid://c2g0pw5amocod")
+
+@export var fade_duration: float = 0.5
+
+const DIM_DISTANCE = 580.0 
+const END_LIMIT = 600.0 
 
 @onready var progress_bar: TextureProgressBar = $ProgressBar
+@onready var glowhead: TextureRect = $ProgressBar/Glowhead
 
 func _ready() -> void:
+	modulate.a = 1.0 
 	progress_bar.value = 0
+	
+	glowhead.position.y = (progress_bar.size.y / 2) - (glowhead.size.y / 2) + 3.5
+	
 	start_loading_animation()
+
+func _process(delta: float) -> void:
+	var percent = 0.0
+	
+	if progress_bar.max_value > 0:
+		percent = progress_bar.value / progress_bar.max_value
+	
+	var bar_width = progress_bar.size.x
+	var target_x = percent * bar_width
+	
+	glowhead.position.x = target_x - (glowhead.size.x / 2)
+	
+	var start_dim_x = END_LIMIT - DIM_DISTANCE
+	
+	if glowhead.position.x >= END_LIMIT:
+		glowhead.modulate.a = 0.0
+	elif glowhead.position.x > start_dim_x:
+		var distance_into_fade = glowhead.position.x - start_dim_x
+		var opacity = 1.0 - (distance_into_fade / DIM_DISTANCE)
+		glowhead.modulate.a = opacity
+	else:
+		glowhead.modulate.a = 1.0
 
 func start_loading_animation() -> void:
 	var tween = create_tween()
-	
-	tween.tween_property(progress_bar, "value", 100, 3.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)	
+	tween.tween_property(progress_bar, "value", 100, 3.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT) 
 	tween.finished.connect(_on_loading_complete)
 
 func _on_loading_complete() -> void:
-	get_tree().change_scene_to_packed(MAIN)
+	var fade_tween = create_tween()
+	fade_tween.tween_property(self, "modulate:a", 0.0, fade_duration)
+	fade_tween.tween_callback(_change_to_main_scene)
+
+func _change_to_main_scene() -> void:
+	get_tree().change_scene_to_packed(MAIN_GAME_SCENE)
