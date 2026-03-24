6bee0b3cef0c97f8b1b08632d5a8cf68d12cb55e
Kiri <cultivatingghoul@gmail.com>
2026-03-24 10:57:24 +0200

    added  coin pickup sound

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,19 @@
 extends Area2D
 
+@onready var sound: AudioStreamPlayer2D = $Sound
+
 var collected = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == 'Attendant' and not collected:
 		collected = true
 		GameManager.current_coins += 1
-		call_deferred("queue_free")
+		
+		hide()
+		
+		set_deferred("monitoring", false)
+		
+		sound.play()
+		await sound.finished
+		
+		queue_free()
